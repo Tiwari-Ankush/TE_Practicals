dc5e3135206a6d2ec2a173f5b2d8e92647add543
Ankush Tiwari <92021625+TiwariBro@users.noreply.github.com>
2024-03-04 01:20:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+def google_app_engine_practical():
+    print("Google App Engine Practical")
+    
+    # Dummy operations
+    num1 = 10
+    num2 = 5
+    addition = num1 + num2
+    subtraction = num1 - num2
+    multiplication = num1 * num2
+    division = num1 / num2
+    
+    return addition, subtraction, multiplication, division
+
+# Call the function
+results = google_app_engine_practical()
+print("Results of operations:", results)
